e527126bcf699fefb9cc080505cc870676b45400
Junko <junko03enoshima@gmail.com>
2025-10-21 09:02:33 -0500

    Solucionar problema de pÃ¡gina main vacÃ­a: agregar contenido y redirecciÃ³n automÃ¡tica a home

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    // Redirigir automáticamente a home
+    this.router.navigate(['/main/home']);
   }
 
 }
