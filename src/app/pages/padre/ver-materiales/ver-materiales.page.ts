762aeeb344fb624759b1a285c330c981843ed556
xd <108909719+TuDigitadorPorUnDiaPIPIPI@users.noreply.github.com>
2025-10-22 18:17:07 -0500

    Todo de padre en estaticos

---
+++ src/app/pages/padre/ver-materiales/ver-materiales.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-ver-materiales',
+  templateUrl: './ver-materiales.page.html',
+  styleUrls: ['./ver-materiales.page.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class VerMaterialesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
