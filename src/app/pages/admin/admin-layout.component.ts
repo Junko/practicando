e0f09c4228d54282398ec2705488efb7f94cfb72
Junko <junko03enoshima@gmail.com>
2025-10-24 02:41:31 -0500

    Corregir errores de compilaciÃ³n - convertir AdminLayoutComponent a NgModule

--- src/app/pages/admin/admin-layout.component.ts
+++ src/app/pages/admin/admin-layout.component.ts
@@ -1,50 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { 
-  IonApp, 
-  IonContent, 
-  IonHeader, 
-  IonMenu, 
-  IonMenuButton, 
-  IonRouterOutlet, 
-  IonTitle, 
-  IonToolbar,
-  IonList,
-  IonItem,
-  IonIcon,
-  IonLabel
-} from '@ionic/angular/standalone';
 import { Firebase } from '../../services/firebase';
 import { Utils } from '../../services/utils';
-import { HeaderComponent } from '../../shared/components/header/header.component';
-import { CustomTabsComponent } from '../../shared/components/custom-tabs/custom-tabs.component';
 import { ADMIN_TABS_CONFIG } from '../../shared/configs/tabs-configs';
 import { TabsConfig } from '../../shared/models/tab-config.model';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss'],
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    IonApp,
-    IonContent,
-    IonHeader,
-    IonMenu,
-    IonMenuButton,
-    IonRouterOutlet,
-    IonTitle,
-    IonToolbar,
-    IonList,
-    IonItem,
-    IonIcon,
-    IonLabel,
-    HeaderComponent,
-    CustomTabsComponent
-  ]
+  styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent {
   firebaseSvc = inject(Firebase);
