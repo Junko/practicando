8c3832c5985c749d1de387e1e08988babc55bec7
Junko <junko03enoshima@gmail.com>
2025-10-28 22:40:28 -0500

    Eliminar pÃ¡gina ver-usuario y limpiar rutas duplicadas

--- src/app/pages/usuarios-crud/usuarios-crud-routing.module.ts
+++ src/app/pages/usuarios-crud/usuarios-crud-routing.module.ts
@@ -7,14 +7,6 @@ const routes: Routes = [
   {
     path: '',
     component: UsuariosCrudPage
-  },
-  {
-    path: 'ver-usuario',
-    loadChildren: () => import('./ver-usuario/ver-usuario.module').then( m => m.VerUsuarioPageModule)
-  },
-  {
-    path: 'ver-usuario',
-    loadChildren: () => import('./ver-usuario/ver-usuario.module').then( m => m.VerUsuarioPageModule)
   }
 ];
 
